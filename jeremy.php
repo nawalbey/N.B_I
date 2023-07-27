aece6a62f01fb96a1c7fe174408b5e805faaf1bf
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-07-27 11:28:49 +0200

    projet nawal

---
+++ jeremy.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <form class="flex1">
+        <ul class="flex">
+            <li>
+                <label for="name">first name&nbsp;:</label><br>
+                <input type="text" id="name" name="user_name" />
+            </li>
+            <li>
+                <label for="mail">last name&nbsp;:</label><br>
+                <input type="email" id="mail" name="user_mail" />
+            </li>
+            <li>
+                <label for="msg">E-mail&nbsp;:</label><br>
+                <textarea id="msg" name="user_message"></textarea>
+            </li>
+        </ul>
+
+    </form>
+</body>
+
+</html>
